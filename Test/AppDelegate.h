345bc2a91b8cd86f040f1506e1b0ed8c2260ccf5
chudley2112 <kevin.walters@zachrycorp.com>
2017-02-01 13:51:14 -0600

    Initial Commit

---
+++ Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by Kevin Walters on 2/1/17.
+//  Copyright © 2017 Kevin Walters. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
